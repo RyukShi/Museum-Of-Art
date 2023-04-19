eccce472cf40dc7290b3ea9117b9c819a676a9c7
RyukShi <aure16300P@gmail.com>
2023-04-19 16:16:44 +0200

    State entity created

---
+++ src/Repository/StateRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\State;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<State>
+ *
+ * @method State|null find($id, $lockMode = null, $lockVersion = null)
+ * @method State|null findOneBy(array $criteria, array $orderBy = null)
+ * @method State[]    findAll()
+ * @method State[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class StateRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, State::class);
+    }
+
+    public function save(State $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(State $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}

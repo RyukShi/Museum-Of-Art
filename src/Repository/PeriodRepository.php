95dc4e9a481facdca36bfbd5e214d9e9be5cadb0
RyukShi <aure16300P@gmail.com>
2023-04-19 15:27:42 +0200

    Period entity created

---
+++ src/Repository/PeriodRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Period;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Period>
+ *
+ * @method Period|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Period|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Period[]    findAll()
+ * @method Period[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PeriodRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Period::class);
+    }
+
+    public function save(Period $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Period $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
